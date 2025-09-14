0b451bbd9ee27e4583f9878f5b71f3325d3e6d34
KriptoBaba382 <199w29c@mail.ru>
2025-09-14 21:06:32 +0300

    add refactoring

--- script.js
+++ script.js
@@ -13,90 +13,119 @@
 // let ServicePercentPrice;
 
 const appData = {
-    title: "",
-    screens: "",
-    screenPrice: 0,
-    adaptive: true,
-    service1: "",
-    service2: "",
-    allServicePrices: 0,
-    rollback: 15,
-    fullPrice: 0,
-    ServicePercentPrice: 0,
-    asking: function () {
-        this.title = prompt("Как называется ваш проект?", "Калькулятор верстки");
-        this.screens = prompt(
-            "Какие типы экранов нужно разработать?",
-            "Простые, Сложные, Интерактивные"
-        );
+  title: "",
+  screens: [],
+  adaptive: true,
+  allServicePrices: 0,
+  services: {},
+  rollback: 15,
+  fullPrice: 0,
+  ServicePercentPrice: 0,
+  asking: function () {
+    this.title = prompt("Как называется ваш проект?", "Калькулятор верстки");
+    this.askScreens();
+    this.adaptive = confirm("Нужен ли адаптив на сайте?");
+    this.askServicePrices();
+  },
+  isNumber: function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+  },
+  getAllServicePrices() {
+    let sum = 0;
+    for (const key in this.services) {
+      sum += this.services[key];
+    }
+    return sum;
+  },
+  askScreens() {
+    for (let i = 0; i < 2; i++) {
+      const name = this.stringValidatePrompt(
+        "Какие типы экранов надо разработать?",
+        "простые, сложные, невозможные"
+      );
+      const price = +this.numberValidatePrompt("Сколько это будет стоить?");
+      this.screens.push({
+        id: i,
+        name,
+        price,
+      });
+    }
+  },
+  askServicePrices() {
+    for (let i = 0; i < 2; i++) {
+      const name = this.stringValidatePrompt(
+        "Какой дополнительный тип услуги нужен?"
+      );
 
-        this.screenPrice = +prompt("Сколько будет стоить данная работа?");
-        while (!this.isNumber(this.screenPrice)) {
-            this.screenPrice = +prompt("Сколько будет стоить данная работа?");
-        }
-        this.adaptive = confirm("Нужен ли адаптив на сайте?");
-        this.allServicePrices = this.getAllServicePrices();
-        this.fullPrice = this.getFullPrice()
-        console.log(this.getRollbackMessage());
-    },
-    isNumber: function (num) {
-        return !isNaN(parseFloat(num)) && isFinite(num);
-    },
-    getAllServicePrices() {
-        let sum = 0;
-        for (let i = 0; i < 2; i++) {
-            if (i === 0) {
-                this.service1 = prompt("Какой дополнительный тип услуги нужен?");
-            } else if (i === 1) {
-                this.service2 = prompt("Какой дополнительный тип услуги нужен?");
-            }
-            sum += +prompt("Сколько это будет стоить?");
-        }
-        return sum;
+      this.services[`${name}__${i}`] = +this.numberValidatePrompt(
+        "Сколько это будет стоить?"
+      );
+    }
+  },
+
+  numberValidatePrompt(text, ansDefault = "") {
+    return this.validatePromt(text, this.isNumber, ansDefault);
+  },
+  stringValidatePrompt(text, ansDefault = "") {
+    return this.validatePromt(
+      text,
+      (val) => typeof val === "string" && !this.isNumber(val),
+      ansDefault
+    );
+  },
+
+  validatePromt(text, validateFn, ansDefault = "") {
+    let val = prompt(text, ansDefault);
+    while (!validateFn(val)) {
+      alert("Значение не верно! Введите верное значение");
+      val = prompt(text);
+    }
+    return val;
+  },
+
+  getRollbackMessage:
+    function (/* Эта функция принимает входной параметр - цена (price) */) {
+      if (this.fullPrice >= 30000) {
+        return "Даем скидку в 10%";
+      }
+      if (this.fullPrice >= 15000 && this.fullPrice < 30000) {
+        return "Даем скидку в 5%";
+      }
+      if (this.fullPrice < 1500 && this.fullPrice > 0) {
+        return "Скидка не предусмотрена";
+      }
+      if (this.fullPrice <= 0) {
+        return "Скидка не предусмотрена";
+      }
     },
 
-    getRollbackMessage: function (/* Эта функция принимает входной параметр - цена (price) */) {
-        console.log(this.fullPrice)
-        if (this.fullPrice >= 30000) {
-            return "Даем скидку в 10%";
-        }
-        if (this.fullPrice >= 15000 && this.fullPrice < 30000) {
-            return "Даем скидку в 5%";
-        }
-        if (this.fullPrice < 1500 && this.fullPrice > 0) {
-            return "Скидка не предусмотрена";
-        }
-        if (this.fullPrice <= 0) {
-            return "Скидка не предусмотрена";
-        }
-    },
-    getFullPrice() {
-        return this.screenPrice + this.allServicePrices;
-    },
-    getTitle() {
-        return this.title.charAt(0).toUpperCase() + this.title.slice(1);
-    },
-    getServicePercentPrices() {
-        return this.fullPrice - this.fullPrice * (this.rollback / 100);
-    },
-    start: function () {
-        this.asking();
-        this.logger();
-    },
-    logger: function () {
-        for (let i in this) {
-            console.log(this[i]);
-        }
-    },
+  get screenPrice() {
+    return this.screens.reduce((acc, screen) => {
+      acc += screen.price;
+      return acc;
+    }, 0);
+  },
+  getFullPrice() {
+    console.log(this.screenPrice);
+    return this.screenPrice + this.allServicePrices;
+  },
+  getTitle() {
+    return this.title.charAt(0).toUpperCase() + this.title.slice(1);
+  },
+  getServicePercentPrices() {
+    return this.fullPrice - this.fullPrice * (this.rollback / 100);
+  },
+  start: function () {
+    this.asking();
+    this.allServicePrices = this.getAllServicePrices();
+    this.fullPrice = this.getFullPrice();
+    this.logger();
+  },
+  logger: function () {
+    console.log(this.services);
+    console.log(this.screens);
+    console.log(this.allServicePrices);
+    console.log(this.fullPrice);
+  },
 };
 appData.start();
-// fullPrice = getFullPrice();
-// title = getTitle();
-// appData.ServicePercentPrice = getServicePercentPrices();
-// const servicePercentPrice = getServicePercentPrices(
-//   appData.fullPrice,
-//   appData.rollback
-// );
-
-// console.log(appData.fullPrice);
-// console.log(appData.ServicePercentPric
